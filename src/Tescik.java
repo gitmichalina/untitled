089e64e8c9414a7ec7bf1606350826cf9d2bc01a
gitmichalina <michalina.prog@gmail.com>
2022-01-15 19:07:26 +0100

    czwarty commit

--- src/Tescik.java
+++ src/Tescik.java
@@ -1,5 +1,5 @@
 public class Tescik {
     public static void main(String[] args) {
-        System.out.println("Hellobbbb2 World!");
+        System.out.println("Hellobbbbfgbfgbfgbb2 World!");
     }
 }
